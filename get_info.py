6ab02996a08303afbce3efe77955daca01d52ac0
Vibilan Jayanth <vibilan@firstindo.com>
2024-01-26 22:41:28 +0800

    guru, siswa_l, siswa_p, rombongan_belajar, kurikulum, penyelenggaraan, mbs, semester_data

--- get_info.py
+++ get_info.py
@@ -22,8 +22,38 @@ def get_detail_dapodik(data):
     return akreditasi, kepala_sekolah, operator
 
 
+def get_summary_1(data):
+    font_list = data.find_all('font')
+    guru = font_list[0].text
+    siswa_l = font_list[1].text
+    siswa_p = font_list[2].text
+    rombongan_belajar = font_list[3].text
+    return guru, siswa_l, siswa_p, rombongan_belajar
+
+
+def get_summary_2(data):
+    font_list = data.find_all('font')
+    kurikulum = font_list[0].text.replace('\xa0', ' ')
+    penyelenggaraan = data.text.split("Penyelenggaraan : ")[-1].split("\n")[0]
+    
+    mbs_icon = data.find('i', class_="glyphicon-check")
+    mbs = str(mbs_icon is not None)
+
+    semester_data = font_list[1].text
+    return kurikulum, penyelenggaraan, mbs, semester_data
+
+
+def get_summary_3(data):
+    pass
+
+
+def get_summary_4(data):
+    pass
+
+
 def get_info_for_link(link):
-    response = requests.get(link, timeout=3)
+    # response = requests.get(link)
+    response = requests.get(link, timeout=5)
     soup = BeautifulSoup(response.content, 'html.parser')
     body = soup.body
 
@@ -42,7 +72,25 @@ def get_info_for_link(link):
     info["kepala_sekolah"] = kepala_sekolah
     info["operator"] = operator
 
+    summary_divs = body.find_all('div', class_ = "row")[2].find_all('div')
+    
+    guru, siswa_l, siswa_p, rombongan_belajar = get_summary_1(summary_divs[0])
+    info["guru"] = guru
+    info["siswa_l"] = siswa_l
+    info["siswa_p"] = siswa_p
+    info["rombongan_belajar"] = rombongan_belajar
+
+    kurikulum, penyelenggaraan, mbs, semester_data = get_summary_2(summary_divs[1])
+    info["kurikulum"] = kurikulum
+    info["penyelenggaraan"] = penyelenggaraan
+    info["mbs"] = mbs 
+    info["semester_data"] = semester_data
+
     print(info)
+
+    _ = get_summary_3(summary_divs[2])
+    _ = get_summary_4(summary_divs[3])
+
     return info
 
 
