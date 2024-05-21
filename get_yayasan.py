d61ec0172326baa3872889586cf6482e3c851fb0
Vibilan Jayanth <vibilan@firstindo.com>
2024-05-21 17:07:06 +0800

    fix some errors and use pickle

--- get_yayasan.py
+++ get_yayasan.py
@@ -1,4 +1,5 @@
 import csv
+import pickle
 
 import aiohttp
 import asyncio
@@ -7,8 +8,10 @@ from bs4 import BeautifulSoup
 
 
 BASE_URL = "https://referensi.data.kemdikbud.go.id/pendidikan/npsn/"
+HEADER_1 = ["npsn", "naungan", "npyp", "link"]
 
 
+# IO
 def read_from_csv(filename):
     rows = []
     with open(filename, newline='\n') as file:
@@ -26,7 +29,18 @@ def write_to_csv(header, data, filename):
         wr.writerows(data)
 
 
-def get_yayasan_info(page):
+def write_to_pickle(list_, filename):
+    with open(filename, "wb") as fp:
+        pickle.dump(list_, fp)
+
+
+def read_from_pickle(filename):
+    with open(filename, 'rb') as fp:
+        return pickle.load(fp)
+
+
+# NPSN -> Yayasan
+def get_yayasan_for_school(page):
     soup = BeautifulSoup(page, "lxml")
     body = soup.body
 
@@ -37,47 +51,61 @@ def get_yayasan_info(page):
     return naungan, npyp, link
 
 
-async def scrape_page(id, session):
+async def scrape_school(id, session):
     url = BASE_URL + id
     try:
         async with session.get(url) as response:
             if response.status != 200:
                 return [id, None, None, None]
             result = await response.text()
-            info = get_yayasan_info(result)
+            info = get_yayasan_for_school(result)
             return [id, *info]
     except Exception as _:
         return [id, None, None, None]
     
 
-async def scrape_all_pages(part):
+async def scrape_all_schools(part):
     _, schools = read_from_csv("csv/yayasan/private_foundation_owned.csv")
 
     step = 3000
     start = (part - 1) * step
     end = part * step
-    ids = ids[start:end]
+    schools = schools[start:end]
 
     my_conn = aiohttp.TCPConnector(limit = 100, ssl = False)
     async with aiohttp.ClientSession(connector = my_conn) as session:
         tasks = []
         for school in schools:
-            task = asyncio.ensure_future(scrape_page(school[0], session))
+            task = asyncio.ensure_future(scrape_school(school[0], session))
             tasks.append(task)
         info = await asyncio.gather(*tasks, return_exceptions = True)
 
-    header = ["npsn", "naungan", "npyp", "link"]
-    write_to_csv(header, info, f"yayasan_{part}.csv")
+    write_to_pickle(info, f"school_{part}.pickle")
     return info
 
 
+def combine_npsn_to_yayasan(filename):
+    data = []
+    for i in range(1, 7):
+        data += read_from_pickle(f"school_{i}.pickle")
+    write_to_csv(HEADER_1, data, filename)
+
+
 def run():
-    import requests
-    
+    # TESTING INDIVIDUAL PAGES FOR SCHOOL
+    # import requests
     # link = "https://referensi.data.kemdikbud.go.id/tabs.php?npsn=69988051"
     # link = "https://referensi.data.kemdikbud.go.id/tabs.php?npsn=10200410"
-    link = "https://referensi.data.kemdikbud.go.id/tabs.php?npsn=10703149"
+    # link = "https://referensi.data.kemdikbud.go.id/tabs.php?npsn=10703149"
+    # page = requests.get(link, timeout=5, verify = False)
+    # info = get_yayasan_info(page.text)
+    # print(info)
 
-    page = requests.get(link, timeout=5, verify = False)
-    info = get_yayasan_info(page.text)
-    print(info)
+    # GETTING SCHOOL INFO
+    asyncio.run(scrape_all_schools(1))
+    # asyncio.run(scrape_all_schools(2))
+    # asyncio.run(scrape_all_schools(3))
+    # asyncio.run(scrape_all_schools(4))
+    # asyncio.run(scrape_all_schools(5))
+    # asyncio.run(scrape_all_schools(6))
+    # combine_npsn_to_yayasan("npsn_to_yayasan.csv")
